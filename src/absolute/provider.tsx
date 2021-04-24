4b164d77cdce32b3260bb7ca16fa3ddd66dd283b
Suhan Moon <leader@trabricks.io>
2021-04-24 20:19:23 +0900

    import fix

--- src/absolute/provider.tsx
+++ src/absolute/provider.tsx
@@ -1,10 +1,10 @@
-import React, { PropsWithChildren, useCallback, useState } from 'react';
+import * as React from 'react';
 import { AbsoluteContext } from './context';
 import { AbsoluteProps, Container, Props, ViewItem } from '../utils/elements';
 
 const AbsoluteProvider: React.FC<Props> = props => {
-  const [screens, setScreens] = useState<{ [key: string]: PropsWithChildren<AbsoluteProps> }>({});
-  const attach = useCallback((target: string, props: PropsWithChildren<AbsoluteProps> | undefined) => {
+  const [screens, setScreens] = React.useState<{ [key: string]: React.PropsWithChildren<AbsoluteProps> }>({});
+  const attach = React.useCallback((target: string, props: React.PropsWithChildren<AbsoluteProps> | undefined) => {
     if (!props) return;
     setScreens(screens => ({
       ...screens,
@@ -17,10 +17,9 @@ const AbsoluteProvider: React.FC<Props> = props => {
       <AbsoluteContext.Provider value={{ attach }}>{props.children}</AbsoluteContext.Provider>
       {Object.keys(screens)
         .filter(v => !!screens[v])
-        .map((key: string) => {
-          const props = screens[key];
-          return <ViewItem key={`ab${key}`} {...props} />;
-        })}
+        .map((key: string) => (
+          <ViewItem key={`ab${key}`} {...screens[key]} />
+        ))}
     </Container>
   );
 };
